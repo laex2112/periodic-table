a76a1ac249d413fe31a2202ced6bf7c0b9405cdf
laex0727 <khangdao2112@gmail.com>
2024-04-24 02:25:47 +0700

    adjusted footer and prettier formatted

--- src/components/App.js
+++ src/components/App.js
@@ -21,127 +21,159 @@ import "../components/styles/elements.css"
 import GitHubButton from "react-github-btn"
 
 class App extends Component {
-  state = {
-    showInfo: false,
-    element: {}
-  }
-
-  showInfo = num => {
-    this.setState({ showInfo: true, element: elements[num] })
-  }
-
-  closeInfo = () => {
-    this.setState({ showInfo: false })
-  }
-
-  /**
-   * Helper method that will generate a number of elements
-   * from start index to end index to avoid copy pasting
-   * all the hundred elements
-   * @param {number} start
-   * @param {number} end
-   * @return {Array<Object>}
-   */
-  populateElements = (start, end) => {
-    let items = []
-    for (let i = start; i <= end; i++) {
-      items.push(<Element showInfo={this.showInfo} num={i} />)
+    state = {
+        showInfo: false,
+        element: {},
     }
-    return items
-  }
 
-  render() {
-    let {
-      name,
-      summary,
-      symbol,
-      category,
-      number,
-      source,
-      appearance,
-      atomic_mass,
-      molar_heat,
-      density,
-      melt,
-      boil
-    } = this.state.element
+    showInfo = (num) => {
+        this.setState({ showInfo: true, element: elements[num] })
+    }
 
-    return (
-      <div className="wrapper">
-        <div id="table">
-          {/* Elements 1-4 */}
-          {this.populateElements(1, 4)}
-          {/* Information Table */}
-          {this.state.showInfo && (
-            <Fragment>
-              <div id="element-box" className={`${category}`}>
-                <div className="number">{number}</div>
-                <div className="symbol">{symbol}</div>
-                <div className="element-name">{name}</div>
-              </div>
-              <div id="information">
-                <div
-                  onClick={this.closeInfo}
-                  className="close-button"
-                  title="Close Info"
-                >
-                  Close [&times;]
+    closeInfo = () => {
+        this.setState({ showInfo: false })
+    }
+
+    /**
+     * Helper method that will generate a number of elements
+     * from start index to end index to avoid copy pasting
+     * all the hundred elements
+     * @param {number} start
+     * @param {number} end
+     * @return {Array<Object>}
+     */
+    populateElements = (start, end) => {
+        let items = []
+        for (let i = start; i <= end; i++) {
+            items.push(<Element showInfo={this.showInfo} num={i} />)
+        }
+        return items
+    }
+
+    render() {
+        let {
+            name,
+            summary,
+            symbol,
+            category,
+            number,
+            source,
+            appearance,
+            atomic_mass,
+            molar_heat,
+            density,
+            melt,
+            boil,
+        } = this.state.element
+
+        return (
+            <div className="wrapper">
+                <div id="table">
+                    {/* Elements 1-4 */}
+                    {this.populateElements(1, 4)}
+                    {/* Information Table */}
+                    {this.state.showInfo && (
+                        <Fragment>
+                            <div id="element-box" className={`${category}`}>
+                                <div className="number">{number}</div>
+                                <div className="symbol">{symbol}</div>
+                                <div className="element-name">{name}</div>
+                            </div>
+                            <div id="information">
+                                <div
+                                    onClick={this.closeInfo}
+                                    className="close-button"
+                                    title="Close Info"
+                                >
+                                    Close [&times;]
+                                </div>
+                                <div>
+                                    <h1 className="big_title">{name}</h1>
+                                    <span className={`cat_name ${category}`}>
+                                        {category}
+                                    </span>
+                                    {appearance && (
+                                        <div className="appearance">
+                                            <strong>Appearance:</strong>{" "}
+                                            {appearance}
+                                        </div>
+                                    )}
+                                    <div className="atom_info">
+                                        <span>
+                                            Atomic Mass: {atomic_mass} |{" "}
+                                        </span>
+                                        <span>Density: {density}</span>
+                                        {molar_heat && (
+                                            <span>
+                                                {" "}
+                                                | Molar Heat: {molar_heat}
+                                            </span>
+                                        )}
+                                        {melt && <span> | Melt: {melt}K</span>}
+                                        {boil && <span> | Boil: {boil}K</span>}
+                                    </div>
+                                    <div>
+                                        {summary} ...{" "}
+                                        <a target="_blank" href={source}>
+                                            Source
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </Fragment>
+                    )}
+                    {/* Populating elements from 5-57 */}
+                    {this.populateElements(5, 57)}
+                    {/* Lanthanoids split 72-89 */}
+                    {this.populateElements(72, 89)}
+                    {/* Actinoids split 104-119*/}
+                    {this.populateElements(104, 118)}
+                    {/* Lanthenoids 58-71*/}
+                    {this.populateElements(58, 71)}
+                    {/* Actionoids 90-103 */}
+                    {this.populateElements(90, 103)}
                 </div>
-                <div>
-                  <h1 className="big_title">{name}</h1>
-                  <span className={`cat_name ${category}`}>{category}</span>
-                  {appearance && (
-                    <div className="appearance">
-                      <strong>Appearance:</strong> {appearance}
-                    </div>
-                  )}
-                  <div className="atom_info">
-                    <span>Atomic Mass: {atomic_mass} | </span>
-                    <span>Density: {density}</span>
-                    {molar_heat && <span> | Molar Heat: {molar_heat}</span>}
-                    {melt && <span> | Melt: {melt}K</span>}
-                    {boil && <span> | Boil: {boil}K</span>}
-                  </div>
-                  <div>
-                    {summary} ...{" "}
-                    <a target="_blank" href={source}>
-                      Source
-                    </a>
-                  </div>
-                </div>
-              </div>
-            </Fragment>
-          )}
-          {/* Populating elements from 5-57 */}
-          {this.populateElements(5, 57)}
-          {/* Lanthanoids split 72-89 */}
-          {this.populateElements(72, 89)}
-          {/* Actinoids split 104-119*/}
-          {this.populateElements(104, 118)}
-          {/* Lanthenoids 58-71*/}
-          {this.populateElements(58, 71)}
-          {/* Actionoids 90-103 */}
-          {this.populateElements(90, 103)}
-        </div>
-        <footer className="footer center">
-          <span>
-            2022, Put together by{" "}
-            <a href="https://tamalchowdhury.com" target="_blank" rel="noopener">
-              Tamal Chowdhury
-            </a>
-          </span>
-          <GitHubButton
-            href="https://github.com/tamalweb/periodic-table"
-            data-color-scheme="no-preference: light; light: light; dark: dark;"
-            data-show-count="true"
-            aria-label="Star tamalweb/periodic-table on GitHub"
-          >
-            Star
-          </GitHubButton>
-        </footer>
-      </div>
-    )
-  }
+                <footer className="footer center">
+                    <span className="barcode">727</span>
+                    <span>
+                        An ElectronJS fork of{" "}
+                        <a
+                            href="https://github.com/tamalchowdhury/periodic-table"
+                            target="_blank"
+                            rel="noopener"
+                        >
+                            periodic-table
+                        </a>{" "}
+                        by{" "}
+                        <a
+                            href="https://github.com/laex2112"
+                            target="_blank"
+                            rel="noopener"
+                        >
+                            Dao Thinh Khang
+                        </a>{" "}
+                        and{" "}
+                        <a
+                            href="https://github.com/khokhonguyen"
+                            target="_blank"
+                            rel="noopener"
+                        >
+                            Nguyen Thanh Khoi
+                        </a>
+                    </span>
+                    <GitHubButton
+                        href="https://github.com/laex2112/periodic-table"
+                        data-color-scheme="no-preference: light; light: light; dark: dark;"
+                        data-icon="octicon-comment-discussion"
+                        data-size="large"
+                        aria-label="Discuss laex2112/periodic-table on GitHub"
+                    >
+                        Discuss
+                    </GitHubButton>
+                </footer>
+            </div>
+        )
+    }
 }
 
 export default App
